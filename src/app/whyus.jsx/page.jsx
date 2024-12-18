0ebc3e3abb7fde1b574ae1a97f62a8677a2de5dc
justin-ike05 <chiju2005@gmail.com>
2024-12-18 19:15:24 +0100

    feat: add text

--- src/app/whyus.jsx/page.jsx
+++ src/app/whyus.jsx/page.jsx
@@ -19,7 +19,7 @@ const WhyUs = () => {
         Choose Planet Wheels for high-quality products, fast and easy booking, and exceptional customer service. We&apos;re dedicated to providing a seamless and satisfying experience every time!
       </p>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-5'>
-        <WhyUsCard icon={icon1} title="Financing Options" description={'Planet Wheels offers top-quality wheels at competitive prices, paired with a seamless shopping experience and expert support. With a wide selection to fit every need and a commitment to sustainability, we deliver style, durability, and value you can trust.'} />
+        <WhyUsCard icon={icon1} title="Financing Options" description={'Planet Wheels offers top-quality cars at competitive prices, paired with a seamless shopping experience and expert support. With a wide selection to fit every need and a commitment to sustainability, we deliver style, durability, and value you can trust.'} />
         <WhyUsCard icon={icon2} title="Satisfied Customers" description={'We prioritize customer satisfaction by offering high-quality products, fast shipping, secure payments, and excellent customer service. Your happiness is our top priority!'} />
         <WhyUsCard icon={icon3} title="Fast & Easy Booking" description={'We make booking easy and fast. Our simple, user-friendly platform ensures a smooth experience, allowing you to quickly find and reserve what you need with just a few clicks. Enjoy convenience and speed every time you book with us!'} />
       </div>
